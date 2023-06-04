e330f37d71fb38ad5f7a72352e9c4e1eda05b3a1
Selcukk70 <106600043+Selcukk70@users.noreply.github.com>
2023-06-04 21:13:01 +0200

    Added

---
+++ Intro-to-Vue-3-L3-start/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            img_description: 'this is a example',
+            url: 'https://www.google.com/'
+        }
+    }
+})
